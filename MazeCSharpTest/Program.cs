8cbd1dc108119ce32e76d5c61107674542538fc8
xiangmu110 <xiangmu110@qq.com>
2016-01-28 16:48:19 +0800

    find path is over

--- MazeCSharpTest/Program.cs
+++ MazeCSharpTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using Random = System.Random;
@@ -32,38 +33,33 @@ namespace MazeCSharpTest
     {
         public int x = -1;
         public int y = -1;
+        /// <summary>
+        /// 是否有墙
+        /// </summary>
         public bool hasWall = true;
+        /// <summary>
+        /// 墙的类型
+        /// </summary>
         public WallType type = WallType.None;
+        /// <summary>
+        /// 状态
+        /// </summary>
+        public int flag = 0;
 
-        public void Set(int x, int y, bool hasWall = true, WallType type = WallType.None)
+        public void Set(int x, int y, bool hasWall = true, int flag = 0, WallType type = WallType.None)
         {
             this.x = x;
             this.y = y;
             this.hasWall = hasWall;
             this.type = type;
+            this.flag = flag;
         }
-
         public override string ToString()
         {
-            return "[WallInfo:x=" + x + " y=" + y + " hasWall=" + hasWall + " type=" + type + "]";
+            return "[WallInfo:x=" + x + " y=" + y + " hasWall=" + hasWall + " type=" + type + " flag=" + flag + "]";
         }
     }
-    public class BlockWallInfo
-    {
-        public WallInfo info { get; private set; }
-        public WallType wallDirect { get; private set; }
-        public BlockWallInfo(WallInfo info, WallType wallDirect)
-        {
-            this.info = info;
-            this.wallDirect = wallDirect;
-        }
-
-        public override string ToString()
-        {
-            return "[BlockWallInfo:wallDirect=" + wallDirect + " info=" + info + "]";
-        }
-    }
-    class Maze
+    public class Maze
     {
         /// <summary>
         /// 迷宫宽
@@ -89,6 +85,36 @@ namespace MazeCSharpTest
 
         }
 
+        /// <summary>
+        /// 墙总数
+        /// </summary>
+        public int Count
+        {
+            get
+            {
+                return _maze.Count;
+            }
+        }
+        /// <summary>
+        /// 获取墙的序号
+        /// </summary>
+        /// <param name="info">墙信息</param>
+        /// <returns></returns>
+        public int GetIndex(WallInfo info)
+        {
+            return info.x + info.y * _x;
+        }
+        /// <summary>
+        /// 获取指定节点的序号
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
+        public int GetIndex(int x, int y)
+        {
+            return x + y * _x;
+        }
+        
         /// <summary>
         /// 创建迷宫
         /// </summary>
@@ -139,7 +165,7 @@ namespace MazeCSharpTest
         /// </summary>
         /// <param name="x">横向坐标</param>
         /// <param name="y">纵向坐标</param>
-        /// <returns>迷宫墙信息</returns>
+        /// <returns>迷宫墙信息，不存在则返回null</returns>
         public WallInfo this[int x, int y]
         {
             get
@@ -159,11 +185,50 @@ namespace MazeCSharpTest
                 }
             }
         }
+        /// <summary>
+        /// 获取指定序号墙信息
+        /// </summary>
+        /// <param name="index">指定编号</param>
+        /// <returns>如果不存在则返回null</returns>
+        public WallInfo this[int index]
+        {
+            get
+            {
+                if (index >= _maze.Count || index < 0)
+                {
+                    return null;
+                }
+                return _maze[index];
+            }
+            private set
+            {
+                if (index >= _maze.Count || index < 0)
+                {
+                    throw new Exception("Maze>this[int index]:index is OutOfRange to _maze.");
+                }
+                _maze[index] = value;
+            }
+        }
+        /// <summary>
+        /// 使用A*寻路查找路径
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="end"></param>
+        /// <returns></returns>
+        public List<WallInfo> FindPath(WallInfo start, WallInfo end)
+        {
+            return _findPath.FindPath(this, start, end);
+        }
 
         #region Other Tool Function
 #if !OtherTool
         public string SelectWall(WallInfo info)
         {
+            if (info.flag == 1)
+            {
+                return "●";
+            }
+
             string ch = string.Empty;
             switch (info.type)
             {
@@ -234,8 +299,25 @@ namespace MazeCSharpTest
 #endif
         #endregion
         #region Private
+        private class BlockWallInfo
+        {
+            public WallInfo info { get; private set; }
+            public WallType wallDirect { get; private set; }
+            public BlockWallInfo(WallInfo info, WallType wallDirect)
+            {
+                this.info = info;
+                this.wallDirect = wallDirect;
+            }
+
+            public override string ToString()
+            {
+                return "[BlockWallInfo:wallDirect=" + wallDirect + " info=" + info + "]";
+            }
+        }
         private Random _rand;
         private List<WallInfo> _maze = new List<WallInfo>();
+        private AStarFindPath _findPath = new AStarFindPath();
+
         private void InitList()
         {
             int nLerp = _x * _y - _maze.Count;
@@ -252,6 +334,7 @@ namespace MazeCSharpTest
                 _maze.RemoveRange(0, -nLerp);
             }
 
+            //初始化
             //设置第一个
             _maze[0].Set(0, 0);
             //设置其他
@@ -470,6 +553,217 @@ namespace MazeCSharpTest
         #endregion
     }
 
+    public class AStarFindPath
+    {
+        public class Node
+        {
+            public WallInfo wallInfo { get; set; }
+            public int valSrc { get; set; }
+            public int valDis { get; set; }
+            public int valSum { get { return valDis + valSrc; } }
+            public Node parent { get; set; }
+            public Node(WallInfo info)
+            {
+                this.wallInfo = info;
+
+                this.valDis = 0;
+                this.valSrc = 0;
+                this.parent = null;
+            }
+            public Node() : this(null)
+            {
+            }
+
+            public override string ToString()
+            {
+                return "[Node:valSrc=" + valSrc + " valDis" + valDis + " valSum" + valSum + " wallInfo=" + wallInfo + "]";
+            }
+        }
+
+        /// <summary>
+        /// 查找路径
+        /// </summary>
+        /// <param name="maze"></param>
+        /// <param name="wStart"></param>
+        /// <param name="wStop"></param>
+        /// <param name="bMode"></param>
+        /// <returns></returns>
+        public List<WallInfo> FindPath(Maze maze, WallInfo wStart, WallInfo wStop, bool bMode = false)
+        {
+            //初始化寻路节点列表
+            InitList(maze);
+
+            //起始节点
+            Node startNode = _nodeList[maze.GetIndex(wStart)];
+            Node endNode = _nodeList[maze.GetIndex(wStop)];
+
+            //未检查列表
+            List<Node> openNode = new List<Node>();
+            //已检查列表
+            List<Node> closeNode = new List<Node>();
+
+            //初始化检查列表
+            openNode.Add(startNode);
+
+            Node currentNode = null;
+            Node tempNode = null;
+            while (openNode.Count > 0)
+            {
+                //设置当前结点
+                currentNode = openNode[0];
+
+                //取得最优节点
+                foreach (var item in openNode)
+                {
+                    if (item.valSum < currentNode.valSum || //总权值最优
+                        item.valSum == currentNode.valSum && item.valDis < currentNode.valDis)//或者相同情况下距目标最哦近
+                    {
+                        currentNode = item;
+                    }
+                }
+
+                //将最优节点从未检测列表中删除，并添加到已检测列表中
+                openNode.Remove(currentNode);
+                closeNode.Add(currentNode);
+
+                //如果当前结点为结束节点
+                if (currentNode == endNode)
+                {//生成并返回路径
+                    return GenericPath(startNode, endNode);
+                }
+
+                //访问周边节点
+                for (int x = -1, y = -1; x <= 1; x++)
+                {
+                    for (y = -1; y <= 1; y++)
+                    {
+                        if (x == 0 && y ==0)
+                        {//不访问自身节点
+                            continue;
+                        }
+
+                        if (!bMode && x == y)
+                        {//不访问斜对角节点
+                            continue;
+                        }
+
+                        //获取周边节点实例
+                        tempNode = _nodeList[maze.GetIndex(currentNode.wallInfo.x + x, currentNode.wallInfo.y + y)];
+
+                        if(tempNode.wallInfo.hasWall || closeNode.Contains(tempNode))
+                        {//当节点为墙或已在已访问列表中时，跳出当前循环
+                            continue;
+                        }
+
+                        //计算当前结点到startNode的权值
+                        int nValSrc = currentNode.valSrc + GetNodeDistance(currentNode, tempNode);
+
+                        if (!openNode.Contains(tempNode) || nValSrc < tempNode.valSrc)
+                        {//如果不在未检测列表中或距离起点更近
+                            tempNode.valSrc = nValSrc;//更新权值
+                            tempNode.valDis = GetNodeDistance(endNode, tempNode);//更新权值
+                            tempNode.parent = currentNode;//设置父节点
+
+                            if (!openNode.Contains(tempNode))
+                            {//如果没有在检测列表中
+                                openNode.Add(tempNode);
+                            }
+                        }
+                    }
+                }
+
+                currentNode = null;
+                tempNode = null;
+            }
+
+            ClearList();//清理列表
+            return null;
+        }
+
+        #region Privite
+        /// <summary>
+        /// 迷宫数据扩展后得到的寻路节点列表
+        /// </summary>
+        private List<Node> _nodeList = new List<Node>();
+        /// <summary>
+        /// 初始化寻路节点列表
+        /// </summary>
+        /// <param name="maze"></param>
+        private void InitList(Maze maze)
+        {
+            int nLerp = maze.Count - _nodeList.Count;
+            if (nLerp > 0)
+            {
+                while (nLerp > 0)
+                {
+                    _nodeList.Add(new Node());
+
+                    --nLerp;
+                }
+            }
+            else if (nLerp < 0)
+            {
+                _nodeList.RemoveRange(0, -nLerp);
+            }
+
+            for (int index = 0, length = _nodeList.Count; index < length; index++)
+            {
+                _nodeList[index].wallInfo = maze[index];
+            }
+        }
+        /// <summary>
+        /// 清理寻路节点列表
+        /// </summary>
+        private void ClearList()
+        {
+            foreach (var item in _nodeList)
+            {
+                item.parent = null;
+                item.wallInfo = null;
+                item.valDis = 0;
+                item.valSrc = 0;
+            }
+        }
+        private List<WallInfo> GenericPath(Node startNode, Node endNode)
+        {
+            List<WallInfo> path = new List<WallInfo>();
+
+            Node temp = endNode;
+            while (temp != startNode)
+            {//由后向前遍历链表
+                path.Add(temp.wallInfo);
+                temp = temp.parent;
+            }
+
+            //反转列表
+            path.Reverse();
+
+            ClearList();//清理列表
+            return path;
+        }
+        /// <summary>
+        /// 获取两个节点之间的权值
+        /// </summary>
+        /// <param name="currentNode"></param>
+        /// <param name="tempNode"></param>
+        /// <returns></returns>
+        private int GetNodeDistance(Node currentNode, Node tempNode)
+        {
+            int cntX = Math.Abs(currentNode.wallInfo.x - tempNode.wallInfo.x);
+            int cntY = Math.Abs(currentNode.wallInfo.y - tempNode.wallInfo.y);
+
+            if (cntX > cntY)
+            {
+                return cntY * 14 + (cntX - cntY) * 10;
+            }
+            else
+            {
+                return cntX * 14 + (cntY - cntX) * 10;
+            }
+        }
+        #endregion
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -479,22 +773,46 @@ namespace MazeCSharpTest
             int x, y;
             do
             {
+                Console.Write("请输入迷宫宽和高（空格分开）：");
                 string[] arg = Console.ReadLine().Split(' ');
                 if (arg.Length != 2)
                 {
                     Console.WriteLine("Arg error >length != 2");
                     continue;
                 }
-                x = int.Parse(arg[0]);
-                y = int.Parse(arg[1]);
+                if (!int.TryParse(arg[0], out x))
+                {
+                    Console.WriteLine("x 输入错误！例：2 3");
+                    continue;
+                }
+                if (!int.TryParse(arg[1], out y))
+                {
+                    Console.WriteLine("y 输入错误！例：2 3");
+                    continue;
+                }
 
                 stopwatch.Start();
                 maze.Create(x, y);
                 stopwatch.Stop();
-
-                string msg = String.Format("x = {0} y = {1} Create Time(ms) > {2}",x,y, stopwatch.Elapsed.TotalMilliseconds);
-                Console.WriteLine(msg);
+                double createTime = stopwatch.Elapsed.TotalMilliseconds;
                 stopwatch.Reset();
+
+                List<WallInfo> path = null;
+                stopwatch.Start();
+                path=maze.FindPath(maze[1,1], maze[maze._x - 2, maze._y - 2]);
+                stopwatch.Stop();
+                double findTime = stopwatch.Elapsed.TotalMilliseconds;
+                stopwatch.Reset();
+                if (path != null)
+                {
+                    foreach (var item in path)
+                    {
+                        item.flag = 1;
+                    }
+                }
+
+                string msg = String.Format("x = {0} y = {1} Create Time(ms) > {2} Find Time(ms) > {3}", x, y, createTime, findTime);
+                Console.WriteLine(msg);
 
                 for (int h = maze._y - 1; h >= 0; h--)
                 {
